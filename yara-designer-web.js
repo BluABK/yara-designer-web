8b7628c63b8fde596ab56a86c1fbb493850283cb
BluABK <bluabk@bluabk.net>
2020-05-11 23:29:42 +0200

    Seems to be in a working state again.

--- yara-designer-web.js
+++ yara-designer-web.js
@@ -1,3 +1,5 @@
+const path = require("path");
+const ejs = require("ejs");
 require("dotenv").config();
 
 const BIND_HOST = process.env.BIND_HOST;
@@ -6,8 +8,36 @@ const BIND_PORT = process.env.BIND_PORT;
 const express = require('express');
 const app = express();
 
+// Prepare dotenv vars.
+const baseUrl = process.env.BACKEND_HOST;
+
+
+// Send in JavaScript vars from dotenv.
+var ejsRenderedHtmlIndex;
+ejs.renderFile(__dirname + '/src/static/index.html',
+    {
+        thisIsATest: "'" + process.env.BIND_HOST + "'",
+        POST_RULE_ROUTE: `'${baseUrl}${process.env.POST_RULE_ROUTE}'`,
+        POST_COMMIT_ROUTE: `'${baseUrl}${process.env.POST_COMMIT_ROUTE}'`,
+        GET_RULES_ROUTE: `'${baseUrl}${process.env.GET_RULES_ROUTE}'`,
+        GET_RULE_ROUTE: `'${baseUrl}${process.env.GET_RULE_ROUTE}'`
+    }, function(err, str) {
+    // str => Rendered HTML string
+    // console.log(str);
+    ejsRenderedHtmlIndex = str;
+});
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/src/static'));
+
+app.use('/static', express.static(path.join(__dirname, '/src/static')));
+app.use(express.static('/src/static/scripts/designer.js'), );
+
+
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/src/index.html');
+    res.send(ejsRenderedHtmlIndex);
+    // res.sendFile(__dirname + '/src/static/index.html');
 });
 
 
